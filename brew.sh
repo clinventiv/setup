2a49ec159f9bbdda8abff9f607c4a9707d589b85
Lyons <christopher.lyons@lmc02gh32ydrjd.gswa.tld>
2015-10-19 16:47:24 -0400

    added sublime and sass to setup

--- brew.sh
+++ brew.sh
@@ -11,12 +11,47 @@ brew tap caskroom/cask
 brew install brew-cask
 
 # main tools | more at http://caskroom.io/search
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install spectacle
+brew cask install iterm2 sublime-text spectacle mamp
+
+# sass
+gem install sass
+
+# nvm, node, npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install stable
+nvm use stable
+
+# npm packages
+npm install -g yo bower grunt grunt-cli gulp
+npm install -g generator-webapp generator-angular generator-jhipster
 
 
 # setup aliases
 cd $HOME
 touch .secrets
 echo 'source ~/Documents/_setup/aliases-functions' >> .secrets
+
+# sublime packages
+# https://packagecontrol.io/installation#st2
+
+# git
+# gitgutter
+# emmet
+# docblokr
+# sublimelinder
+# Bootstrap 3 Snippets
+# jquery snippets
+# jquery
+# sass
+# scss
+# brackethighlighter
+# color highlighter
+# alignment
+# livereload
+# SublimeCodeIntel
+# HTML5
+# all autocomplete
+# HTML-CSS-JS Prettify
+# advanced new file
+# angular
