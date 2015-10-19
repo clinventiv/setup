1d86a5a3fff06151b9127cabb2a0a583b9ede753
Lyons <christopher.lyons@lmc02gh32ydrjd.gswa.tld>
2015-10-19 15:59:24 -0400

    Updates to bash

--- brew.sh
+++ brew.sh
@@ -1,5 +1,7 @@
 # yadr. includes homebrew
-sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`" -s ask
+sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
+cd $HOME/.yadr
+rake update
 
 # get homebrew, if not using skwp/dotfiles
 # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -12,3 +14,9 @@ brew install brew-cask
 brew cask install iterm2
 brew cask install sublime-text
 brew cask install spectacle
+
+
+# setup aliases
+cd $HOME
+touch .secrets
+echo 'source ~/Documents/_setup/aliases-functions' >> .secrets
